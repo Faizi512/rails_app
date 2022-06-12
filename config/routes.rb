c10244684d85adfebead17f4755fc29a3c99d0fc
Faizan Haider <hfaizan53@gmail.com>
2022-06-12 23:43:20 +0500

    Footer and risk added

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,5 @@ Rails.application.routes.draw do
   get "/aus", to: "pages#aus"
   get "/terms-and-condition", to:"pages#terms_condition"
   get "/cookie-policy", to:"pages#cookie_policy"
-  get "/risk-and-warnings", to:"pages#risk_and_warnings"
   get "/privacy-policy", to:"pages#privacy_policy"
 end
